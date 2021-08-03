8afe5130ea6df20943c19b74d6e4b2eab580cf95
Ivo Huntjens <ivo.huntjens@devolksbank.nl>
2021-08-03 14:57:38 +0200

    Initial Commit

---
+++ MVVM People List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM People List
+//
+//  Created by Ivo Huntjens on 03/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
