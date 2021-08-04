167177af92498cf33af4e9f14fc0fe27ec1b239c
Ivo Huntjens <ivo.j.huntjens@gmail.com>
2021-08-05 00:49:20 +0200

    Add recovery mechanism to check Core Data database

--- MVVM People List/ViewModel/PersonListViewModel.swift
+++ MVVM People List/ViewModel/PersonListViewModel.swift
@@ -12,6 +12,7 @@ class PersonListViewModel {
     var isDoneLoading = false
     var people = CurrentValueSubject<[Person], Never>([Person]())
     private let api = PeopleAPI()
+    private let context = AppDelegate.instance.persistentContainer.viewContext
     private var subscriptions = Set<AnyCancellable>()
     
     /**
@@ -26,11 +27,42 @@ class PersonListViewModel {
         let page = people.value.count / api.pageSize + 1
         api.fetchPeople(page)
             .last()
+            .tryCatch { _ in
+                Future<PersonResponse, Error> { [unowned self] promise in
+                    let items = try! self.context.fetch(PersonEntity.fetchRequest()) as! [PersonEntity]
+                    let lowerBound = api.pageSize * (page - 1)
+                    let upperBound = min(api.pageSize * page, items.count)
+                    let paged = items.isEmpty ? [] : items[lowerBound..<upperBound]
+                        .map {
+                            return Person(id: $0.id, firstName: $0.firstName!, lastName: $0.lastName!, email: $0.email!)
+                        }
+                    let response = PersonResponse(
+                        page: page,
+                        perPage: api.pageSize,
+                        total: items.count,
+                        totalPages: Int(ceil(Double(items.count) / Double(api.pageSize))),
+                        data: paged
+                    )
+                    promise(.success(response))
+                }.eraseToAnyPublisher()
+            }
             .sink(
                 receiveCompletion: { _ in },
                 receiveValue: { [unowned self] response in
-                    self.people.send(response.data)
-                    self.isDoneLoading = response.page == page
+                    self.people.send([self.people.value, response.data].flatMap { $0 })
+                    self.isDoneLoading = response.page == response.totalPages
+                    
+                    response.data.forEach { person in
+                        let item = PersonEntity.init(context: self.context)
+                        item.email = person.email
+                        item.firstName = person.firstName
+                        item.id = person.id
+                        item.lastName = person.lastName
+                        
+                        context.insert(item)
+                    }
+                    
+                    AppDelegate.instance.saveContext()
             })
             .store(in: &subscriptions)
     }
