1f6b3022a24346dc5d6bdb2782be19fe448a196f
Ivo Huntjens <ivo.j.huntjens@gmail.com>
2021-08-05 00:46:57 +0200

    Make Person#avatar nillable

--- MVVM People List/Model/Person.swift
+++ MVVM People List/Model/Person.swift
@@ -8,9 +8,18 @@
 import Foundation
 
 struct Person : Decodable {
+    let id: Int64
     let firstName: String
     let lastName: String
     let email: String
     lazy var fullName: String = "\(firstName) \(lastName)"
-    let avatar: String
+    let avatar: String?
+    
+    init(id: Int64, firstName: String, lastName: String, email: String) {
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.avatar = nil
+    }
 }
